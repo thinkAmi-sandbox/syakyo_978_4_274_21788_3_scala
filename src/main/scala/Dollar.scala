01da5d118c9972727f9706d6f715f000b17ebd70
thinkAmi <dev.thinkami+git@gmail.com>
2021-01-10 10:58:34 +0900

    第4章

--- src/main/scala/Dollar.scala
+++ src/main/scala/Dollar.scala
@@ -2,7 +2,15 @@ package com.thinkami.tdd
 
 
 class Dollar(
-              var amount: Int // timesで更新できるよう、読み取り専用のvalでなく、書き込みもできるvarを使う
+              // timesで更新できるよう、読み取り専用のvalでなく、書き込みもできるvarを使う
+              // timesでの更新が不要になったので、varからvalへと切り替える
+              // さらに、val/varをつけなくても、暗黙的にprivateなインスタンス変数として作成される
+              // http://www.ne.jp/asahi/hishidama/home/tech/scala/class.html#h_member
+              // ただ、equals()で比較するときにprivateフィールドだとアクセスできない
+              // (Javaの場合、同一クラスであれば、別インスタンスのprivateにアクセスできる模様)
+              // そのため、アクセス修飾子のprivate[T]を使用して、同一クラスならアクセスできるようにする
+              // http://www.ne.jp/asahi/hishidama/home/tech/scala/class.html#h_access_modifiers
+              private[Dollar] val amount: Int
             ) {
   def times(multiplier: Int): Dollar = {
     // Scalaなのでreturnを省略
--- src/test/scala/MoneyTest.scala
+++ src/test/scala/MoneyTest.scala
@@ -10,23 +10,26 @@ class MoneyTest extends AnyFunSuite {
     // Dollarは同じパッケージなのでimportしなくても使える
     val five = new Dollar(5)
 
-    // 再代入できるように、varを使う
-    var product = five.times(2)
-
     // `===` はScalaにないため、使用しているものによって定義されている
     // https://stackoverflow.com/questions/39490236/difference-between-and-in-scala-spark
-    assert(product.amount === 10)
+    // ScalaTestの場合、 `===` を使うほうが、エラーになったときに詳しい情報が出てくる
+    // https://www.scalatest.org/getting_started_with_fun_suite
+    // assert(1 == 2)
+    // => [info]   org.scalatest.exceptions.TestFailedException was thrown. (MoneyTest.scala:28)
+    // assert(1 === 2)
+    // =>  [info]   1 did not equal 2 (MoneyTest.scala:33)
 
-    product = five.times(3)
-    assert(product.amount === 15)
+    assert(new Dollar(10) === five.times(2))
+
+    assert(new Dollar(15) === five.times(3))
   }
 
   test("等価性") {
     // Scalaの場合、equals()を実装していないクラスは、JavaのObject.equals()を呼び出すので、コンパイルエラーにはならない
     // https://qiita.com/hysdsk/items/b37c4cf5ee21bbf1c494
-    assert(new Dollar(5) == (new Dollar(5)))
+    assert(new Dollar(5) === (new Dollar(5)))
 
     // 三角測量用のコード
-    assert(new Dollar(5) != (new Dollar(6)))
+    assert(new Dollar(5) !== (new Dollar(6)))
   }
 }
